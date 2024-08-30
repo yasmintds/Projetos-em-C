370486d313c8655e7cb83430753bc4df9d4f5c83
yasmintds <162698196+yasmintds@users.noreply.github.com>
2024-08-30 08:24:18 -0300

    Add files via upload

---
+++ aula 1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Este é o primeiro exemplo do curso de linguagem C
+int main(void) {
+//Exibi as informações de console
+  
+  printf("Meu primeiro trabalho em c\n");
+  printf("Meu nome é:Yasmin \n");
+  //Sucesso ao compilar o codigo 
+  return 0;
+}
